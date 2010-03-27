6e5522e894f8723bca6de7f7a8ff79e01fa31935
ecavazos <ejcavazos@gmail.com>
2010-03-27 11:08:25 -0700

    cleaning up my j to the s

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,34 +1,22 @@
-$(function () {
-  $(".add-time").click(function (event) {
-    var form = $("#time-entry");
+var Mynute = function () {
+  var formName = "time-entry";
+  this.form = $("#" + formName);
+};
+
+Mynute.prototype = {
+
+  showNewForm: function (event) {
+    var self = event.data;
+    var form = self.form;
     if (form.css("display") == "block") return false;
     form.attr("action", "/time");
     form.show();
-    setDateToToday();
+    self.setDate();
     event.preventDefault();
-  });
+  },
   
-  $(".discard").click(function (event) {
-    $("#time-entry").hide();
-    $("input[id!=submit], textarea").val("");
-    $("option").attr("selected", false);
-    event.preventDefault();
-  });
-  
-  $("#time-entry").submit(function (event) {
-    $.ajax({
-      url: $(this).attr("action"),
-      type: "POST",
-      data: $(this).serialize(),
-      dataType: "html",
-      success: function (result){
-        $(result).replaceAll(".entries");
-      }
-    });
-    event.preventDefault();
-  });
-  
-  $(".edit").click(function (event) {
+  showEditForm: function (event) {
+    var self = event.data;
     $.ajax({
       url: $(this).attr("href"),
       success: function (entry) {
@@ -45,24 +33,74 @@ $(function () {
       }
     });
     event.preventDefault();
-  });
+  },
   
-  $(".delete").click(function (event) {
+  discardForm: function (event) {
+    var self = event.data;
+    self.form.hide();
+    self.resetFormValues();
+    event.preventDefault();
+  },
+  
+  submitForm: function (event) {
+    var self = event.data;
+    $.ajax({
+      url: $(this).attr("action"),
+      type: "POST",
+      data: $(this).serialize(),
+      dataType: "html",
+      success: function (result) {
+        self.postSuccess.call(self, result);
+      }
+    });
+    event.preventDefault();
+  },
+  
+  deleteTimeEntry: function (event) {
+    var self = event.data;
     $.ajax({
       url: $(this).attr("href"),
       type: "DELETE",
       success: function (result) {
         $(result).replaceAll(".entries");
+        self.bindEditAndDelete();
       }
     });
     event.preventDefault();
-  })
+  },
   
-  function setDateToToday() {
+  bindEditAndDelete: function () {
+    $(".edit").bind("click", this, this.showEditForm);
+    $(".delete").bind("click", this, this.deleteTimeEntry);
+  },
+  
+  resetFormValues: function () {
+    $("input[id!=submit], textarea").val("");
+    $("option").attr("selected", false);
+  },
+  
+  postSuccess: function (result) {
+    $(result).replaceAll(".entries");
+    this.resetFormValues();
+    this.bindEditAndDelete();
+  },
+  
+  setDate: function () {
     var today = new Date();
     var dt = (today.getMonth() + 1)
            + "/" + today.getDate()
            + "/" + today.getFullYear();
     $("#date").val(dt);
   }
+};
+
+$(function () {
+  var mynute = new Mynute();
+  
+  $(".add-time").bind("click", mynute, mynute.showNewForm);
+  $(".discard").bind("click", mynute, mynute.discardForm);
+  $("#time-entry").bind("submit", mynute, mynute.submitForm);
+  $(".edit").bind("click", mynute, mynute.showEditForm);
+  $(".delete").bind("click", mynute, mynute.deleteTimeEntry);
+
 });
